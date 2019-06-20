101b4178f2d37592003fc16a2897278c7546cad9
Laurie Tendulkar <lauriemail@protonmail.com>
2019-06-20 18:47:20 +0200

    Added socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <ul className="icons">
+          <li><a href="https://twitter.com/theenglishbookshop1" target="_blank" rel="noreferrer" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+          <li><a href="https://www.facebook.com/theenglishbookshop/" target="_blank" rel="noreferrer" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+          <li><a href="https://www.instagram.com/theenglish_bookshop/" target="_blank" rel="noreferrer" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+          <li><a href="MAILTO:mail@theenglishbookshop.org" target="_blank" rel="noreferrer" className="icon fa-envelope"><span className="label">Mail</span></a></li>
+        </ul>
+        <p className="copyright">&copy; The English Bookshop. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
